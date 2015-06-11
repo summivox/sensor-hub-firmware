a60ab0ff8c94e6b6f5b16b2b26ca97407bf8bc97
summivox <summivox@gmail.com>
2015-06-11 16:45:26 -0400

    simplify `chain_stop`

--- user/chain.cpp
+++ user/chain.cpp
@@ -24,32 +24,29 @@ void chain_init() {
     // initialize DMA pointers
     HAL_DMA_Start(chain_hdma_tx, (uint32_t)chain_buf, (uint32_t)&CHAIN_SPI->DR, chain_buf_n);
     HAL_DMA_Start(chain_hdma_rx, (uint32_t)&CHAIN_SPI->DR, (uint32_t)chain_buf, chain_buf_n);
-    __HAL_DMA_DISABLE(chain_hdma_tx);
-    __HAL_DMA_DISABLE(chain_hdma_rx);
-    CHAIN_SPI->CR2 = (SPI_CR2_TXDMAEN | SPI_CR2_RXDMAEN);
     chain_stop();
 
     chain_inited = true;
 }
 
 void chain_transfer() {
-    // enable DMA request and start SPI
+    // enable SPI DMA request and start SPI
     CHAIN_SPI->CR2 = (SPI_CR2_TXDMAEN | SPI_CR2_RXDMAEN);
     CHAIN_SPI->CR1 |= SPI_CR1_SPE;
 }
 
 void chain_stop() {
-    // stop SPI and disable DMA request
+    // stop SPI and disable SPI DMA request
     CHAIN_SPI->CR1 &=~ SPI_CR1_SPE;
     CHAIN_SPI->CR2 = 0;
 
     // reset DMA
-    __HAL_DMA_DISABLE(CHAIN_HSPI.hdmatx);
-    __HAL_DMA_DISABLE(CHAIN_HSPI.hdmarx);
+    __HAL_DMA_DISABLE(chain_hdma_tx);
+    __HAL_DMA_DISABLE(chain_hdma_rx);
     chain_hdma_tx->Instance->CNDTR = chain_buf_n;
     chain_hdma_rx->Instance->CNDTR = chain_buf_n;
-    __HAL_DMA_ENABLE(CHAIN_HSPI.hdmatx);
-    __HAL_DMA_ENABLE(CHAIN_HSPI.hdmarx);
+    __HAL_DMA_ENABLE(chain_hdma_tx);
+    __HAL_DMA_ENABLE(chain_hdma_rx);
 
     // reset SPI
     CHAIN_SPI_RESET;
